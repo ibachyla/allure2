fdd936b620160b9087e1d63e7b8b41dd965d2687
dtargonsky <dtargons@ITS.JNJ.com>
2017-08-02 10:39:40 +0300

    Add mappings plugin

--- allure-generator/src/main/javascript/index.js
+++ allure-generator/src/main/javascript/index.js
@@ -19,11 +19,13 @@ import './plugins/tab-groups';
 import './plugins/tab-graph';
 import './plugins/tab-timeline';
 import './plugins/tab-performance';
+import './plugins/tab-mapping';
 
 import './plugins/widget-status';
 import './plugins/widget-severity';
 import './plugins/widget-duration';
 import './plugins/widget-groups';
+import './plugins/widget-mappings';
 
 import './plugins/widget-summary';
 import './plugins/widget-history-trend';
